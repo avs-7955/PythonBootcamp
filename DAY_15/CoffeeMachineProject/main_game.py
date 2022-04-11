0960a228dbb64d4211dec92aae09073e5cc443b0
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-04-11 11:59:44 +0530

    DAY_15 game-program

--- DAY_15/CoffeeMachineProject/main_game.py
+++ DAY_15/CoffeeMachineProject/main_game.py
@@ -29,3 +29,59 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+money = 0
+
+
+def make_coffee(order):
+    resources["water"] -= MENU[order]["ingredients"]["water"]
+    resources["milk"] -= MENU[order]["ingredients"]["milk"]
+    resources["coffee"] -= MENU[order]["ingredients"]["coffee"]
+    print("Here is your latte. Enjoy!")
+
+
+def process_coins(order):
+    global money
+    print("Please insert the coins.")
+    quarters = int(input("How many quarters?"))
+    dimes = int(input("How many dimes?"))
+    nickles = int(input("How many nickles?"))
+    pennies = int(input("How many penniess?"))
+    money_deposited = (quarters * 0.25) + (dimes * 0.10)
+    money_deposited += (nickles * 0.05) + (pennies * 0.01)
+    (nickles * 0.05) + ((pennies * 0.01))
+    if(money_deposited > MENU[order]['cost']):
+        money += MENU[order]['cost']
+        refund = round((money_deposited - MENU[order]['cost']), 2)
+        if(refund > 0):
+            print(f"Here is ${refund} dollars in change.")
+        make_coffee(order)
+    else:
+        print("Sorry, that's not enough money. Money refunded.")
+        return
+
+
+flag = 1
+while(flag):
+    user_input = input(
+        "What would you like? (espresso/latte/cappuccino/): ").lower()
+
+    if(user_input == 'report'):
+        for resource in resources:
+            print(f"{resource.title()}: {resources[resource]}")
+        print(f"Money: ${money}")
+
+    elif(user_input != 'report' and user_input != 'off'):
+        print(MENU[user_input]['ingredients'])
+        if(MENU[user_input]["ingredients"]["water"] < resources["water"]):
+            if(MENU[user_input]["ingredients"]["milk"] < resources["milk"]):
+                if(MENU[user_input]["ingredients"]["coffee"] < resources["coffee"]):
+                    process_coins(user_input)
+                else:
+                    print("Sorry there is not enough coffee.")
+            else:
+                print("Sorry there is not enough milk.")
+        else:
+            print("Sorry there is not enough water.")
+
+    elif(user_input == 'off'):
+        flag = 0
