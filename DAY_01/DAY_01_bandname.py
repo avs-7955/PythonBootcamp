398043b6c112af0179af49e26d88b0f20559c671
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-06-18 21:46:49 +0530

    Revert "deletion of files"
    This reverts commit 97fa65abb516ff7b39ce707e8b150623ce789b1e.

---
+++ DAY_01/DAY_01_bandname.py
@@ -0,0 +1,13 @@
+print("Welcome to the Band Name Generator.")
+
+
+print("What's the name of the city you grew up in?")
+city_name = input()
+# Taking the name of the city as input from the user
+
+print("What's your pet's name?")
+pet_name = input()
+# Taking the pet's name as input from the user
+
+print("Your band name could be "+city_name + " " + pet_name+".")
+# Concatenating the city_name and pet_namename to get the band name
---
+++ DAY_01/DAY_01_namelen.py
@@ -0,0 +1,9 @@
+print("Enter your name")
+name = input()
+# Taking the name of the user
+
+print(len(name))
+# The len() function returns the length of the string.
+
+# To get all this done in 1 line
+#print( len( input("Enter your name. ") ) )
