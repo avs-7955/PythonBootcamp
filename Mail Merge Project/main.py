22127639ff45708330cc35c28b61789c0bbf9b53
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-07-02 10:26:42 +0530

    Improving readability

--- Mail Merge Project/main.py
+++ Mail Merge Project/main.py
@@ -1,11 +1,16 @@
 PLACEHOLDER = "[name]"
+# Opening the file and extracting all the lines and appending it to a list.
 with open("./Mail Merge Project/Input/Names/invited_names.txt")as file:
     names = file.readlines()
 
+
 with open("./Mail Merge Project/Input/Letters/starting_letter.txt") as file:
+    # Saving the letter content
     letter_content = file.read()
     for name in names:
+        # Stripping the name so that no extra space is available
         stripped_name = name.strip()
         new_letter_content = letter_content.replace(PLACEHOLDER, stripped_name)
+        # Using f-string to make the file name for each seperate name
         with open(f"./Mail Merge Project/Output/ReadyToSend/letter_for_{stripped_name}.txt", mode='w') as file:
             file.write(new_letter_content)
